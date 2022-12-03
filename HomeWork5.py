d72fcdc7903c51a0c5b2fc747f862d8f26898188
TimurIbatulin <mel0072007@icloud.com>
2022-12-04 00:28:38 +0300

    Третья задача

--- HomeWork5.py
+++ HomeWork5.py
@@ -31,28 +31,40 @@
 # 3. Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
 # Ввод: [1, 1, 2, 3, 4, 4, 4]
 # Вывод: [2, 3]
-list = input('Введите через пробел последовательность чисел - ').split(' ')
-list2 = []
-e = len(list)
-for x in range(e):
-    count = 0
-    f = x +1
-    for y in range(e):
-        if x != y:
-            if list[x] == list[y]:
-                count = count + 1
-    if count == 0:
-        list2.append(list[x])
-print('Ввод:',list)
-print('Вывод:',list2)
-
-
-
+# list = input('Введите через пробел последовательность чисел - ').split(' ')
+# list2 = []
+# e = len(list)
+# for x in range(e):
+#     count = 0
+#     f = x +1
+#     for y in range(e):
+#         if x != y:
+#             if list[x] == list[y]:
+#                 count = count + 1
+#     if count == 0:
+#         list2.append(list[x])
+# print('Ввод:',list)
+# print('Вывод:',list2)
 
 # 4. Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
 # Пример:
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 # - k=4 => 8*(x**4) + 9*(x**3) + 1*(x**2) + 5*x + 4 = 0 или 8*(x**4) + 5*x + 4 = 0 и т.д.
+import random
+k = int(input('Введите значение натуральной степени к - '))
+index = 0
+r = 0
+s = str()
+j = str()
+for x in range(k):
+    k = k - index
+    index = 1
+    j = str(k)
+    r = str(random.randint(0, 100))
+    s = s + r + '*' + '(x**' + j + ')' + '+'
+r = str(random.randint(0, 100))
+s = s + r + ' = 0'
+print(s)
 
 
 
