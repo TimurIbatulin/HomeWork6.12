1088bde79dbcce3df687c98ed13bf98c4b106280
TimurIbatulin <mel0072007@icloud.com>
2022-12-06 23:04:25 +0300

    23

--- HomeWork5.py
+++ HomeWork5.py
@@ -6,66 +6,69 @@
 #     Ввод: 0.001
 #     Вывод: 3.141
 
-# import math
-# y = input('Введите с какой точностью показать число π - ')
-# z = y.split('.')
-# l = len(z[1])
-# p = round(math.pi, l)
-# print(p)
+import math
+y = input('Введите с какой точностью показать число π - ')
+z = y.split('.')
+l = len(z[1])
+p = round(math.pi, l)
+print(p)
 
 # 2. Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 
 
-# n = int(input('Введите число от 2 вкючительно- '))
-# count = 0
-# r = 0
-# for x in range(2,n):
-#     if x != n:
-#         r = n%x
-#         if r == 0:
-#             count +=1
-# if count > 0:
-#     print('число составное')
-# else: print('число простое')
+n = int(input('Введите число от 2 вкючительно- '))
+count = 0
+r = 0
+for x in range(2,n):
+    if x != n:
+        r = n%x
+        if r == 0:
+            count +=1
+if count > 0:
+    print('число составное')
+else: print('число простое')
         
-# 3. Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
-# Ввод: [1, 1, 2, 3, 4, 4, 4]
-# Вывод: [2, 3]
-# list = input('Введите через пробел последовательность чисел - ').split(' ')
-# list2 = []
-# e = len(list)
-# for x in range(e):
-#     count = 0
-#     f = x +1
-#     for y in range(e):
-#         if x != y:
-#             if list[x] == list[y]:
-#                 count = count + 1
-#     if count == 0:
-#         list2.append(list[x])
-# print('Ввод:',list)
-# print('Вывод:',list2)
+3. Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+Ввод: [1, 1, 2, 3, 4, 4, 4]
+Вывод: [2, 3]
+list = input('Введите через пробел последовательность чисел - ').split(' ')
+list2 = []
+e = len(list)
+for x in range(e):
+    count = 0
+    f = x +1
+    for y in range(e):
+        if x != y:
+            if list[x] == list[y]:
+                count = count + 1
+    if count == 0:
+        list2.append(list[x])
+print('Ввод:',list)
+print('Вывод:',list2)
 
 # 4. Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
 # Пример:
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 # - k=4 => 8*(x**4) + 9*(x**3) + 1*(x**2) + 5*x + 4 = 0 или 8*(x**4) + 5*x + 4 = 0 и т.д.
 
-# import random
-# k = int(input('Введите значение натуральной степени к - '))
-# index = 0
-# r = 0
-# s = str()
-# j = str()
-# for x in range(k):
-#     k = k - index
-#     index = 1
-#     j = str(k)
-#     r = str(random.randint(0, 100))
-#     s = s + r + '*' + '(x**' + j + ')' + '+'
-# r = str(random.randint(0, 100))
-# s = s + r + ' = 0'
-# print(s)
+import random
+k = int(input('Введите значение натуральной степени к - '))
+index = 0
+r = 0
+s = str()
+j = str()
+for x in range(k):
+    k = k - index
+    index = 1
+    j = str(k)
+    r = str(random.randint(0, 100))
+    s = s + r + '*' + '(x**' + j + ')' + '+'
+r = str(random.randint(0, 100))
+s = s + r + ' = 0'
+print(s)
+
+
+# Даны два файла, в каждом из которых находится запись многочлена. Задача - сформировать файл, содержащий сумму многочленов (складываются числа, у которых "х" в одинаковых степенях).
 
 
 q = input('Введите первый многочлен - ')
@@ -92,41 +95,33 @@ count = 0
 while len(s) >= 1:
     box = s[0].split('^')
     step = box[1]
-    # print(step)
     box = box[0]
     liter = box[-1]
-    # print(f'liter = ', liter)
     if len(box) == 1:
             index = 1
     elif len(box) > 1:
             index = box[:-1]
     index = box[:-1]
-    # print(f'index = ', index)
     y = 1
     count = 0
    
     while len(s) >= 2:
         box1 = s[y].split('^')
         step1 = box1[1]
-        # print(step1)
         box1 = box1[0]
         liter1 = box1[-1]
-        # print(f'liter1 = ', liter1)
         if len(box1) == 1:
             index1 = 1
         elif len(box1) > 1:
             index1 = box1[:-1]
-        # print(f'index1 = ', index1)
         count = 0
         
         if liter == liter1 and step == step1:
             sum = int(index) + int(index1)
             formula = str(formula) + str(sum) + str(liter) + '^' + str(step)
-            # y = y1
             if sum != 0 and x < plus:
                 formula = str(formula) + '+'
             s.pop(y)
-            # s.pop(0)
             count += 1
             break
         y += 1
@@ -134,70 +129,12 @@ while len(s) >= 1:
             break
     
     if count == 0:
-        formula = str(formula) + str(index) + str(liter) + '^' + str(step) # вписать добавления этой конструкции в формулу
+        formula = str(formula) + str(index) + str(liter) + '^' + str(step)
         if sum != 0 and y < plus:
                 formula = str(formula) + '+'
     s.pop(0)
     
-
 print(formula)
 
 
 
-
-
-# 44x^2+356x^3+-453x^8+87x^5
-# 2x^2+6y^3+-5x^3+x^3
-# 4x^3+x^2+-13x^4
-
-
-
-# q = input('Введите первый многочлен - ')
-# with open('f1.txt', 'w') as f_data:
-#     f_data.write(q)
-# q1 = input('Введите второй многочлен - ')
-# with open('f2.txt', 'w') as f_data:
-#     f_data.write(q1)
-# s = q.split('+')
-# s1 = q1.split('+')
-# t = len(s)
-# print(s)
-# print(s1)
-# for x in range(len(s1)):
-#     s.append(s1[x])
-# print(s)
-# x = 0
-# x1 = t + 1
-# print(x1)
-# y1 = x1
-# plus = x1
-# formula = str()
-# while x < x1:
-#     box = s[x].split('^')
-#     step = box[1]
-#     # print(step)
-#     box = box[0]
-#     liter = box[-1]
-#     # print(f'liter = ', liter)
-#     index = box[:-1]
-#     # print(f'index = ', index)
-#     y = x +1
-#     while y < y1:
-#         box1 = s[x].split('^')
-#         step1 = box1[1]
-#         # print(step1)
-#         box1 = box1[0]
-#         liter1 = box1[-1]
-#         # print(f'liter1 = ', liter1)
-#         index1 = box1[:-1]
-#         # print(f'index1 = ', index1)
-#         if liter == liter1 and step == step1:
-#             sum = int(index) + int(index1)
-#             formula = str(formula) + str(sum) + str(liter) + '^' + str(step)
-#             y = y1
-#             if sum != 0 and x < plus:
-#                 formula = str(formula) + '+'
-#         y += 1
-#     x += 1
-
-# print(formula)
